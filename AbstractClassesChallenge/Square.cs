12eca3f4063457e6a823d384afcec8312a8b9a5a
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-06 11:59:41 -0600

    Updated Classes

--- AbstractClassesChallenge/Square.cs
+++ AbstractClassesChallenge/Square.cs
@@ -5,5 +5,12 @@ namespace AbstractClassesChallenge
     public class Square : Rectangle
     {
         // Implement your Square Class here.
+        public Square(double Side1L, string Name, int NumSides) : base(Side1L, Name, NumSides)
+        {}
+
+        protected override void SetArea()
+        {
+            this.area = Side1L * 2;
+        }
     }
 }
