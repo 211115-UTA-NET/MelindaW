d9d48605ecc660282d227579357444890bc72040
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2021-12-08 08:56:16 -0600

    Add Rock Paper Sissors App

---
+++ RockPaperScissorsApp/RockPaperScissorsApp.App/GameRecord.cs
@@ -0,0 +1,16 @@
+﻿namespace RockPaperScissorsApp.App
+{
+    public class GameRecord
+    {
+        public static int PlayerWins { get; set; } = 0;
+        public static int ComputerWins { get; set; } = 0;
+        public static int Ties { get; set; } = 0;
+        public static string Player { get; set; } = "Player";
+        
+        public static string Record()
+        {
+            string gameScores = $"\n{Player}: {PlayerWins}\nComputer: {ComputerWins}\nTies: {Ties}";
+            return gameScores;
+        }
+    }
+}
